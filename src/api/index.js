82a540c86f10a6b5b1dc4a0de328d0c35e18e978
kangsuroro <kangsuroro2907@gmail.com>
2019-11-14 21:15:11 +0900

    revise server url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const serverApi =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'find-me-server.cibdsxtmnm.ap-northeast-2.elasticbeanstalk.com ';
+  process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://api.find-me.website/';
 
 export const api = axios.create({
   baseURL: serverApi,
