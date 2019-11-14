702a038c48882638daf562bc867dc065859a87f7
kangsuroro <kangsuroro2907@gmail.com>
2019-11-14 21:56:56 +0900

    revise socket url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const serverApi =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://api.find-me.website/';
+  process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://api.find-me.website';
 
 export const api = axios.create({
   baseURL: serverApi,
--- src/api/sockets.js
+++ src/api/sockets.js
@@ -1,2 +1,2 @@
 export const SOCKET_API =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'find-me-server.cibdsxtmnm.ap-northeast-2.elasticbeanstalk.com ';
+  process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://api.find-me.website';
